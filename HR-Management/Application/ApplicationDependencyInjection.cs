aa8a13d2587e6421531a8c51ebcf20cc798b85a1
Fomal9aut <sergeymarchenko903@gmail.com>
2025-08-17 19:48:02 +0300

    added DI services

---
+++ HR-Management/Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IEmployeeHistoryService, EmployeeHistoryService>();
+        services.AddScoped<IDepartmentHistoryService, DepartmentHistoryService>();
+        services.AddScoped<IDepartmentService, DepartmentService>();
+        return services;
+    }
+}
